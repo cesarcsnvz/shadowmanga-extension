dda758939dc6b5ce3a14e5bf0ea2330ae32110ec
cesarcsnvz <cesar.casanova.zu@gmail.com>
2026-04-27 00:37:55 -0500

    Update ShadowManga.kt

--- src/es/shadowmanga/ShadowManga.kt
+++ src/es/shadowmanga/ShadowManga.kt
@@ -1,1 +1,9 @@
+package eu.kanade.tachiyomi.extension.es.shadowmanga
 
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class ShadowManga : Madara(
+    "ShadowManga",
+    "https://shadowmanga.es",
+    "es"
+)
